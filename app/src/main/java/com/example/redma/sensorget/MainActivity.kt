96585e309b5aecc14067c86b7cf6c2fd89a5f6e4
GaZi-NK <redman.fox@icloud.com>
2020-01-02 00:10:26 +0900

    取得したセンサーの一覧表示を着手中

---
+++ app/src/main/java/com/example/redma/sensorget/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.redma.sensorget
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import java.lang.StringBuilder
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //取得したセンサー情報を追加
+        val buffer = StringBuilder()
+
+
+    }
+}
